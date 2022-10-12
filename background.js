d6d9358316a9bc0b69c118a7cdfba5d213b45eb8
LODYZ <38936462+LODYZ@users.noreply.github.com>
2022-10-12 21:14:03 +0200

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener( data => {
-  if ( data.type === 'enableDarkMode' ) {
-	  console.log('aaa');
-	  debugger;
-  }else if( data.type === 'disableDarkMode' ){
-  }
-});
